6d074f8584b6193b159c726e7459de571f7a6389
Žiga Hajšen <ziga.hajsen@gmail.com>
2020-05-12 21:21:18 +0200

    first commit

---
+++ github.js
@@ -0,0 +1,26 @@
+class Github {
+  constructor() {
+    this.client_id = 'c3e12d8934acbece6965';
+    this.client_secret = 'b998a2749a7b6d208b1ddfd8bfb5710ed9c8cddc';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos,
+    };
+  }
+}
